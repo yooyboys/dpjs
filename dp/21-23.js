0fd0a8ec1c7c5d69df75b362b7bc585807530a19
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-12 03:19:17 +0800

    Update 21-23.js

--- dp/21-23.js
+++ dp/21-23.js
@@ -2,15 +2,15 @@
 function adCheck(){
   var myDate = new Date();
   var aaa=myDate.getHours();
-  if(parseInt(aaa)>=21 && parseInt(aaa)<=23){  //时间
+  if(parseInt(aaa)>=20 && parseInt(aaa)<=23){  //时间
     return true;
   }else{
     return false;
   }
 }
   if(adCheck()){
-document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.2/dp/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.2/dp/hls.min.js"><\/script>');
-document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.2/dp/hls/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.2/dp/hls/hls.min.js"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.4/dp/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.4/dp/hls.min.js"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.4/dp/hls/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.4/dp/hls/hls.min.js"><\/script>');
 
   }else{
     //留空
