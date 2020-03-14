baeb983ea91aa4c6c651db72f5443f3e5a22c449
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-14 23:48:13 +0800

    Update 00-03.js

--- dp/00-03.js
+++ dp/00-03.js
@@ -9,7 +9,7 @@ function adCheck(){
   }
 }
   if(adCheck()){
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/hls.min.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/hls.min.js"><\/script>');
 
   }else{
     //留空
