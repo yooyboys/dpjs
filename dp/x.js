b80177a2e24e8b8e287f27b04c39ce0bc8f7b2fc
yooyboys <46861244+yooyboys@users.noreply.github.com>
2020-03-14 23:44:57 +0800

    Update x.js

--- dp/x.js
+++ dp/x.js
@@ -1,16 +1,15 @@
 // 根据屏幕尺寸  
 if (screen && screen.width > 480) {  
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/p2p.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/p2p.js"><\/script>');
+document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/p2p.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/p2p.js"><\/script>');
 
 }  
 
 // userAgent判断  
 if( /Android|webOS|iPhone|Linux|iPod|iPad|BlackBerry/i.test(navigator.userAgent)){  
-document.write('<script src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/hls.min.js"><\/script>');
 document.write('<script src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm/hls.js/dist/hls.min.js"><\/script>');
 
 }  
 
 // 调用
-document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/00-02.js"><\/script>');
-document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.7/dp/20-23.js"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/00-02.js"><\/script>');
+document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/yooyboys/dpjs@1.8/dp/20-23.js"><\/script>');
